275f9172b511becc73d315836b2a1ca5dc4c5438
mustran <ademimusli000@gmail.com>
2020-01-08 15:04:50 +0100

    Add images and email to Card component

--- src/components/CardList/Card/Card.jsx
+++ src/components/CardList/Card/Card.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import styles from "./card.module.css";
 
-const Card = ({ monster }) => {
+const Card = ({ monster, imageKey }) => {
   return (
-    <div>
+    <div className={styles.cardContainer}>
+      <img
+        alt="monster"
+        src={`https://robohash.org/${imageKey}/?set=set2&size=180x180`}
+      />
       <h1>{monster.name}</h1>
+      <p style={{ textAlign: "center" }}>{monster.email}</p>
     </div>
   );
 };
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -5,8 +5,8 @@ import Card from "./Card/Card";
 const CardList = ({ monsters }) => {
   return (
     <div className={styles.cardList}>
-      {monsters.map(monster => (
-        <Card key={monster.id} monster={monster} />
+      {monsters.map((monster, index) => (
+        <Card key={monster.id} monster={monster} imageKey={index + 1}/>
       ))}
     </div>
   );
