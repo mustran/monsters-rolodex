fb2a2e65e9bd7bf335ea5030cdb35802b0f3ea94
mustran <ademimusli000@gmail.com>
2020-01-09 09:50:00 +0100

    Updates

---
+++ precache-manifest.d5df9565463f7e0437c087312b86a6e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6cbc11db4fc0284b98bc1b9b19b5a9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c5939837391d8a687444",
+    "url": "/monsters-rolodex/static/css/main.fcdf31ca.chunk.css"
+  },
+  {
+    "revision": "f20516bb3eea9322c760",
+    "url": "/monsters-rolodex/static/js/2.342e1710.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.342e1710.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5939837391d8a687444",
+    "url": "/monsters-rolodex/static/js/main.c0164fad.chunk.js"
+  },
+  {
+    "revision": "e6506ee2ba66be74f4d3",
+    "url": "/monsters-rolodex/static/js/runtime-main.52116d47.js"
+  }
+]);
